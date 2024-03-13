bd0e1d40ff690e551e1648f3d76eb273a856c222
abooneosoft <148430617+abooneosoft@users.noreply.github.com>
2024-03-13 14:41:53 +0530

    testing updated code

--- LearnJdbc/src/org/rough/Rough1.java
+++ LearnJdbc/src/org/rough/Rough1.java
@@ -6,6 +6,11 @@ public class Rough1 {
 		System.out.println("Hello World");
 		
 	}
+
+	m2(){
+
+		System.out.println("This changes is done in GITHUB for testing");
+	}
 	
 	public static void main(String[] args) {
 		m1();
