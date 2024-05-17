eb260b88b079f642e6af40196f14f16f277d518f
user <user@DESKTOP-P86JAB1>
2024-05-17 12:32:33 +0530

    Api programs

--- GitTest/src/org/test/GitClass1.java
+++
@@ -1,9 +0,0 @@
-package org.test;
-
-public class GitClass1 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello git");
-		
-	}
-}
--- SampleJava/src/org/base/ArrayDup.java
+++ SampleJava/src/org/base/ArrayDup.java
@@ -3,18 +3,20 @@ package org.base;
 public class ArrayDup {
 
 	public static void main(String[] args) {
-		
-		int[] a = {1,2,2,3,3,3};
-		int dupcount = 0;
-		
-		for(int i=0;i<a.length-1;i++) {
-			for(int j =i+1;j<a.length;j++) {
-				if(a[i]==a[j]) {
-					System.out.println(a[i]+" is duplicate");
-					dupcount++;
-				}
 
+		int[] a = { 1, 2, 2, 3, 3, 3 };
+		int dupcount = 0;
+
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] == a[j]) {
+
+					
+					dupcount++;
+					System.out.println(a[i] + " is duplicate");
+				}
 			}
+			
 		}
 	}
 }
